36fa6fefaf6bb6661d9ef5bf7ef9cf8db3cca2e0
codeturk <34295148+musxos@users.noreply.github.com>
2022-10-24 13:52:54 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Index = () => {
 
   useEffect(() => {
     const { REALM } = process.env
-    const mainUrl = REALM ? `/dao/${REALM}` : '/realms'
+    const mainUrl = REALM ? `/dao/MNGO` : '/dao/MNGO'
     router.replace(mainUrl)
   }, [])
 
