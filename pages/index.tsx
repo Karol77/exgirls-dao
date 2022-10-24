39f71985823342abaead5e40cf4962ac47321bb7
codeturk <34295148+musxos@users.noreply.github.com>
2022-10-24 18:54:33 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,9 @@ import { useRouter } from 'next/router'
 const Index = () => {
   const router = useRouter()
   
-  const { REALM } = process.env
-  const mainUrl = REALM ? `/dao/MNGO` : '/dao/MNGO'
-  router.replace(mainUrl)
-  
   useEffect(() => {
-    const { REALM } = process.env
-    const mainUrl = REALM ? `/dao/MNGO` : '/dao/MNGO'
+    const { REALM } = 'MNG'
+    const mainUrl = REALM ? `/dao/${REALM}` : '/realms'
     router.replace(mainUrl)
   }, [])
 
